235a81d437d0393f9eb562c3277db77a207036f5
sergey-uhanov <uhanov.sergio@gmail.com>
2026-04-06 08:10:38 +0300

    add seeds

---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,33 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { UserService } from '../../user/user.service';
+import { JwtPayload } from '@auth/interfaces';
+import type { Request } from 'express';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly userService: UserService
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (req: Request): string => req.cookies.access_token,
+      ]),
+      ignoreExpiration: false,
+      secretOrKey: configService.getOrThrow<string>('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: JwtPayload) {
+    const user = await this.userService.findById(+payload.sub);
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    return user;
+  }
+}
